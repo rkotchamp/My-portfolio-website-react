4c5c6d28f1178468e84b09c033ce4276cdd3b08b
Reuben <treuben90@yahoo.com>
2023-10-02 10:45:55 +0300

    made small changes

--- My-portfolio-website-react/src/Pages/ProjectPage/ProjectPage.jsx
+++ My-portfolio-website-react/src/Pages/ProjectPage/ProjectPage.jsx
@@ -1,4 +1,9 @@
 import Projects from "../../components/ProjectsComponents/Projects";
+import BRinc from "../../assets/My_Tech_logos/Screenshot 2023-09-26 at 4.58.29 AM.png";
+import reactJs from "../../assets/My_Tech_logos/Group 70.png";
+import htmlfile from "../../assets/My_Tech_logos/html5.png";
+import cssFile from "../../assets/My_Tech_logos/css.png";
+import jsFile from "../../assets/My_Tech_logos/Group 68.png";
 
 import "./ProjectPage.css";
 
@@ -11,8 +16,14 @@ function ProjectPage() {
       </div>
       <div className="projects">
         <Projects
-          story="Lambda is a travel plan app where users can be able to share their experience with family and friends and also have a plan of places to be visited"
-          projectName="Lambda Travels"
+          story="There are so many trees being fell down for manufacturing books and even shelves to store books. Upon realisation we decided to create this online library where books can be read and also rented all reducing deforestation and improving our ecosystem at large"
+          projectName="BRinc Library"
+          projectOverview={BRinc}
+          imgSrc1={htmlfile}
+          imgSrc2={cssFile}
+          imgSrc3={jsFile}
+          imgSrc4={reactJs}
+          goGitHub="https://github.com/BRinc-lda/BRinc"
         />
         <Projects />
       </div>
--- My-portfolio-website-react/src/components/ProjectsComponents/Projects.jsx
+++ My-portfolio-website-react/src/components/ProjectsComponents/Projects.jsx
@@ -1,10 +1,20 @@
 import reactLogo from "../../assets/My_Tech_logos/Group 70.png";
 import { useInView } from "react-intersection-observer";
-
 import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
 import "./Project.css";
 
-function Projects({ projectName, story, imgSrc, projectOverview }) {
+function Projects({
+  projectName,
+  story,
+  imgSrc,
+  projectOverview,
+  liveSite,
+  goGitHub,
+  imgSrc1,
+  imgSrc2,
+  imgSrc3,
+  imgSrc4,
+}) {
   const [ref, inView] = useInView();
   return (
     <div>
@@ -19,17 +29,24 @@ function Projects({ projectName, story, imgSrc, projectOverview }) {
           <div className="projectHeaders">
             <h4>{projectName}</h4>
             <div className="projectIcons">
-              <img src={imgSrc} alt="react" />
+              <img src={imgSrc1} alt="html" />
+              <img src={imgSrc2} alt="css" />
+              <img src={imgSrc3} alt="JavaScript" />
+              <img src={imgSrc4} alt="react.js" />
             </div>
           </div>
           <p>{story}</p>
           <div className="gitAndSite">
-            <button className="btn">
-              Code <AiFillGithub />
-            </button>
-            <button className="btn">
-              Live <AiOutlineLink />
-            </button>
+            <a href={goGitHub}>
+              <button className="btn">
+                Code <AiFillGithub />
+              </button>
+            </a>
+            <a href={liveSite}>
+              <button className="btn">
+                Live <AiOutlineLink />
+              </button>
+            </a>
           </div>
         </div>
       </div>
