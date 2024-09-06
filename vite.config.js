94a74568ac6cf8fcf248ca516b1662e1db0d3afe
Reuben <treuben90@yahoo.com>
2024-09-06 14:11:38 +0300

    solved bugs

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true, // helpful for debugging
-    chunkSizeWarningLimit: 1000, // increase limit if it's complaining about chunk size
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // ensures chunking doesn't break
-      },
-    },
-  },
 });
